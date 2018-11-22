c928e515f1b83dce58355276486bcb2a94de88a7
Full Bright <full3right@gmail.com>
2018-11-22 22:29:45 +0100

    Modularize with functions

--- start_scan.sh
+++ start_scan.sh
@@ -2,6 +2,36 @@
 
 DEST=temp/dest
 
+function batch_convert_to_jpg(){
+
+    todayandnow=$1
+    echo "Converting images of batch $todayandnow from tiff to jpg"
+
+    echo "Reduce the weight of the images"
+    for scanfile in temp/$todayandnow*.tiff; do
+        scanfilebase=$(basename "$scanfile")
+        convert "$scanfile" "temp/$scanfilebase.jpg"
+    done
+
+    echo "Conversion finished."
+}
+
+function convert_jpg_to_pdf(){
+
+    echo "Converting jpg from batch $1 to pdf"
+    convert temp/$1*.jpg $DEST/myawesome_scan_$1.pdf
+
+}
+
+function clean_temp_files(){
+
+    todayandnow=$1
+    echo "Remove temporary scanned tiff and jpg files for batch $todayandnow"
+    rm temp/$todayandnow*.jpg
+    rm temp/$todayandnow*.tiff
+
+}
+
 function scan_pages(){
 
     echo "Generate a token for this scan"
@@ -14,14 +44,16 @@ function scan_pages(){
             -l 0 -t 0 -x 210 -y 295
 
     echo "Reduce the weight of the images"
-    for scanfile in temp/$todayandnow.*.tiff; do
-        scanfilebase=$(basename "$scanfile")
-        convert "$scanfile" "temp/$scanfilebase.jpg"
-    done
-    #convert big-image.jpg -strip -quality 90 .jpg
+    #for scanfile in temp/$todayandnow.*.tiff; do
+    #    scanfilebase=$(basename "$scanfile")
+    #    convert "$scanfile" "temp/$scanfilebase.jpg"
+    #done
+    ##convert big-image.jpg -strip -quality 90 .jpg
+    batch_convert_to_jpg "$todayandnow"
 
     echo "Convert the scanned documents into pdf"
-    convert temp/$todayandnow.*.jpg $DEST/myawesome_scan_$todayandnow.pdf && \
+    #convert temp/$todayandnow.*.jpg $DEST/myawesome_scan_$todayandnow.pdf && \
+    convert_jpg_to_pdf "$todayandnow"
 
     echo "Remove temporary scanned tiff files"
     rm temp/$todayandnow*.jpg
@@ -44,13 +76,19 @@ function upload_scannedpages(){
     cd $ORIGINAL_FOLDER
 }
 
-until [[ $REPLY =~ ^[Nn]$ ]]
-do
-  scan_pages
-  read -s -p "Scan another document ? [y/N]: " REPLY
-done
+function interactive_scan(){
 
-# Upload the files to Google Drive
-upload_scannedpages
+    until [[ $REPLY =~ ^[Nn]$ ]]
+    do
+        scan_pages
+        read -s -p "Scan another document ? [y/N]: " REPLY
+    done
+
+    # Upload the files to Google Drive
+    upload_scannedpages
+
+}
+
+#interactive_scan
 
 # End of script
