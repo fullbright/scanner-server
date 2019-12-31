c5c3430bf611d9e98a2c16cf9e8c9e3de4a51e63
Full Bright <full3right@gmail.com>
2019-12-31 14:18:50 +0100

    Adding the usage and customizations

--- scan2pdf.sh
+++ scan2pdf.sh
@@ -4,7 +4,8 @@
 #DEVICE="snapscan"
 ## For network scanners use
 #DEVICE="net:sane.example.org:snapscan"
-DEVICE='brother4:net1;dev0'
+#DEVICE='brother4:net1;dev0'
+DEVICE="hpaio:/net/officejet_4500_g510g-m?ip=192.168.1.25&queue=false"
 
 ## See scanimage --device $(DEVICE) --help
 SOURCES[0]="FlatBed"
@@ -12,6 +13,7 @@ SOURCES[1]="Automatic Document Feeder(left aligned)"
 SOURCES[2]="Automatic Document Feeder(left aligned,Duplex)"
 SOURCES[3]="Automatic Document Feeder(centrally aligned)"
 SOURCES[4]="Automatic Document Feeder(centrally aligned,Duplex)"
+SOURCES[5]="ADF"
 SOURCE=${SOURCES[3]} # Default
 
 RESOLUTIONS=(100 150 200 300 400 600 1200 2400 4800 9600)
@@ -22,11 +24,13 @@ MODES[1]="Gray[Error Diffusion]"
 MODES[2]="True Gray"
 MODES[3]="24bit Color"
 MODES[4]="24bit Color[Fast]"
+MODES[5]="Gray"
 MODE=${MODES[2]}	# Default
 
 function usage()
 {
     echo "This should print the help. Work in progress."
+    echo "scan2pdf.sh -m <mode> -r|d <resolution> -o <outputfilename> -s <source> -k <keep_tmp> -h|?"
 }
 
 function process_option()
